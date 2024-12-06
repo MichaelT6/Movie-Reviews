ca863d10cb2acc1f0fdb49432f5d63b49c3788fa
MichaelT6 <michaeltitalangha6@gmail.com>
2024-12-06 11:31:23 -0600

    first commit

---
+++ routes.py
@@ -0,0 +1,27 @@
+from flask import render_template, request, redirect, url_for
+from app import app, db
+from models import MovieReview
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/data')
+def data():
+    return render_template('data.html')
+
+@app.route('/add_review', methods=['GET', 'POST'])
+def add_review():
+    if request.method == 'POST':
+        new_review = MovieReview(
+            movie_name=request.form['movie_name'],
+            review=request.form['review'],
+            rating=float(request.form['rating'])
+        )
+        
+        db.session.add(new_review)
+        db.session.commit()
+        
+        return redirect(url_for('index'))
+    
+    return render_template('add_review.html')
